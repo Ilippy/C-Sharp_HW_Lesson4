a7ec1483dbcd0f0c67ea78603431516c40804ebd
Ilippy <Ilippyk@gmail.com>
2023-01-12 14:46:52 +0300

    add Task 29 project

---
+++ Task_29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+using NumberMain;
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = CreateArray();
+        Console.WriteLine($"[{String.Join(", ", array)}]");        
+    }
+
+    static int[] CreateArray()
+    {
+        int arrayLength = Numbers.EnterNumber("Введите длину массива");
+        int[] array = new int[arrayLength];
+        Random random = new Random();
+        for (int i = 0; i < arrayLength; i++)
+        {
+            array[i] = random.Next(100);
+        }
+        return array;
+    }
+}
